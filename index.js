940d1fd04b49c886b795cf0a0e8680609e8d8ca6
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2016-02-01 19:11:26 -0300

    add short url and mongodb

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-// '/new/:url*'
+'use strict';
 var http = require('http');
 var app = require('express')();
-process.env.URL = 'http://localhost:3000/';
-var urlContent = { "original_url": null, "short_url": null };
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/url-shortener');
+process.env.URL = process.env.APP_URL ||'http://localhost:3000/';
 
-app.set('views','./views')
+// mongodb schema
+var urlSchema = mongoose.Schema({
+    original_url: String,
+    short_url: String
+});
+
+var Url = mongoose.model('url', urlSchema);
+
+
+// express configs
+app.set('views','./views');
 app.set('view engine','ejs');
 
+// express routes
 app.get('/',function(req,res) {
   res.render('index');
 });
 
+app.get('/:id',function(req,res) {
+  Url.findOne({short_url: process.env.URL + req.params.id},function(err,url) {
+    res.redirect(url.original_url);
+  });
+});
+
 app.get('/new/:url*',function(req,res) {
-  console.log(req.query.allow);
 
-  if (validate(req.url.slice(5))){
-    urlContent.original_url = req.url.slice(5);
-    // add short_url
+  var paramUrl = req.url.slice(5);
+  if (validate(paramUrl)){
+    var result = {};
+    Url.findOne({original_url: paramUrl}).exec().then(function(url) {
+      if (url){
+        result.original_url = url.original_url;
+        result.short_url = url.short_url;
+        res.json(result);
+      } else {
+        var newUrl = {};
+        newUrl.short_url = process.env.URL + randomUrl();
+        newUrl.original_url = paramUrl;
+        var urlContent = new Url(newUrl);
+        urlContent.save(function (err,url) {
+          if (err) return console.error(err);
+          result.original_url = newUrl.original_url;
+          result.short_url = newUrl.short_url;
+          res.json(result);
+        });
+      }
 
-    res.send(urlContent);
+    },function (erro) {
+      console.error(err);
+    });
+
   } else if(req.query.allow == 'true'){
-    urlContent.original_url = 'invalid';
-    urlContent.short_url = (process.env.APP_URL || process.env.URL) + 'zZzZzZz';
+    var result = {}
+    result.original_url = 'invalid';
+    result.short_url = process.env.URL + 'zZzZzZz';
 
-    res.send(urlContent);
+    res.send(result);
   } else {
     res.send({"error":"URL invalid"});
   }
-  res.end();
-  urlContent = { "original_url": null, "short_url": null };
 });
 
 http.createServer(app).listen(3000, function() {
@@ -40,4 +76,23 @@ function validate(url){
   // regex from @diegoperini in https://gist.github.com/dperini/729294
   var regex =  /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
   return regex.test(url);
+}
+
+// mongodb connection check
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected in mongoDB with mongoose');
+});
+
+// Generate random url
+function randomUrl(){
+  var url = "";
+  var VALUES = "ABCDEFabcdef0123456789";
+
+  for (var i=0; i < 4; i++){
+    url += VALUES.charAt(Math.floor(Math.random() * VALUES.length));
+  }
+  
+  return url;
 }
